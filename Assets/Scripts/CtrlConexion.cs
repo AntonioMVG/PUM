34e947a3ffc21603c3f7042aa06fa110fa32a137
AntonioMVG <avilgon223@g.educaand.es>
2022-11-12 13:33:38 +0100

    Conexion con sala existente en otra instancia

--- Assets/Scripts/CtrlConexion.cs
+++ Assets/Scripts/CtrlConexion.cs
@@ -5,37 +5,34 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using Unity.VisualScripting;
 
 
 public class CtrlConexion : MonoBehaviourPunCallbacks
 {
-    #region Variables privadas
-    #endregion
-
-
     #region Variables publicas
-    public GameObject panelInicio; // Panel de inicio del juego
-    public GameObject panelBienvenida; // Panel de bienvenida del juego
-    public GameObject panelCreacionSala; // Panel para creación de una sala
-    public GameObject panelSala; // Panel de la sala
-    public GameObject contenedor; // Contenedor del boton de jugadores
-    public GameObject cajaJugador; // Caja de jugador para la sala
+    public GameObject panelInicio;
+    public GameObject panelBienvenida;
+    public GameObject panelCreacionSala;
+    public GameObject panelSala;
+    public GameObject panelUnirseSala;
+    public GameObject contenedor;
+    public GameObject cajaJugador;
 
-    public TMP_InputField inputNickname; // Entrada con el nombre de usuario
-    public TMP_InputField inputNombreSala; // El usuario introduce el nombre de la sala
-    public TMP_InputField inputMinJug; // El usuario indica el minimo de jugadores
-    public TMP_InputField inputMaxJug; // El usuario indica el maximo de jugadores permitidos
+    public TMP_InputField inputNickname;
+    public TMP_InputField inputNombreSala;
+    public TMP_InputField inputSalaUnirse;
+    public TMP_InputField inputMinJug;
+    public TMP_InputField inputMaxJug;
 
-    public TextMeshProUGUI txtEstado;  // Contiene la última salida por pantalla en estado
-    public TextMeshProUGUI txtInfoUser;  // Contiene información sobre el usuario
-    public TextMeshProUGUI nombreSala; // Nombre de la sala
-    public TextMeshProUGUI tipoSala; // Muestra el tipo de sala
-    public TextMeshProUGUI totalJugadores; // Muestra los jugadores actuales en la sala
-    public TextMeshProUGUI maximoJugadores; // Muestra el máximo de jugadores de la sala
+    public TextMeshProUGUI txtEstado;
+    public TextMeshProUGUI txtInfoUser;
+    public TextMeshProUGUI nombreSala;
+    public TextMeshProUGUI tipoSala;
+    public TextMeshProUGUI totalJugadores;
+    public TextMeshProUGUI maximoJugadores;
 
-    public Button botonConectar; // Instancia del boton Conectar
-    
-
+    public Button botonConectar;
     #endregion
 
     private void Start()
@@ -43,18 +40,11 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         CambiarPanel(panelInicio);
     }
 
-
-
     #region Eventos para botones
-    /// <summary>
-    /// Método que se ejecuta al pulsar el botón de Conexión a Photon
-    /// Comprueba si el nombre de usuario es correcto y realiza la conexión
-    /// </summary>
     public void OnClickConectarAServidor()
     {
         // Comprobamos si el nombre de usuario es correcto
-        if (!(string.IsNullOrWhiteSpace(inputNickname.text) ||
-            string.IsNullOrEmpty(inputNickname.text)))
+        if (!(string.IsNullOrWhiteSpace(inputNickname.text)))
         {
             // Comprobamos si no estamos ya conectados a Photon
             if (!(PhotonNetwork.IsConnected))
@@ -69,19 +59,16 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
             else
             {
                 // Indicar que ya estamos conectados
-                CambiarEstado("Ya está conectado a Photon");
+                CambiarEstado("Conectado a Photon");
             }
-        } else
+        }
+        else
         {
             // Indicar que el nombre no es correcto
             CambiarEstado("Nombre de usuario incorrecto");
         }
     }
 
-    /// <summary>
-    /// Método que se lanza al pulsar el botón Crear Sala del 
-    /// menú de bienvenida. Cambia al panel CreacionSala
-    /// </summary>
     public void OnClickIrACrearSala()
     {
         CambiarPanel(panelCreacionSala);
@@ -103,8 +90,7 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         min = int.Parse(inputMinJug.text);
         max = int.Parse(inputMaxJug.text);
 
-        if (!(string.IsNullOrWhiteSpace(inputNombreSala.text) ||
-            string.IsNullOrEmpty(inputNombreSala.text)))
+        if (!(string.IsNullOrWhiteSpace(inputNombreSala.text)))
         {
             if (min>0 && max >= min)
             {
@@ -126,6 +112,10 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         }
     }
 
+    public void OnClickUnirseASala()
+    {
+        CambiarPanel(panelUnirseSala);
+    }
     #endregion
 
     #region Eventos propios de Photon
@@ -135,7 +125,15 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         CambiarEstado("Conectado a Photon");
         PhotonNetwork.NickName = inputNickname.text;
         txtInfoUser.text = PhotonNetwork.NickName;
-        // Cambiamos a panelBienvenida
+        CambiarPanel(panelBienvenida);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        CambiarEstado("Conectado a Photon");
+        PhotonNetwork.NickName = inputNickname.text;
+        txtInfoUser.text = PhotonNetwork.NickName;
         CambiarPanel(panelBienvenida);
     }
 
@@ -145,34 +143,38 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         CambiarEstado("Sala creada correctamente");
         CambiarPanel(panelSala);
         
-        // Información de los jugadores en sala
-        Player[] jugadores = PhotonNetwork.PlayerList;
-        foreach (var jugador in jugadores)
-        {
-            GameObject botonNuevo = Instantiate(cajaJugador);
-            botonNuevo.transform.SetParent(contenedor.transform);
-            botonNuevo.transform.Find("Nickname").GetComponent<TextMeshProUGUI>().text = jugador.NickName;
+        // Actualizamos los datos de la sala y los jugadores
+        ActualizarSala();
+    }
 
-            //cajaJugador.GetComponentInChildren<TextMeshProUGUI>().text = jugador.NickName;
-            //jugadoresEnSala.text = jugador.ActorNumber.ToString();
-            //jugadoresEnSala.text = jugador.NickName.ToString();
-        }
-        
-        // Información de la Sala
-        nombreSala.text = "Sala " + PhotonNetwork.CurrentRoom.Name.ToString();
-        tipoSala.text = (PhotonNetwork.CurrentRoom.IsOpen ? "Abierta" : "Cerrada");
-        totalJugadores.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString();
-        maximoJugadores.text = PhotonNetwork.CurrentRoom.MaxPlayers.ToString();
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        CambiarEstado("Dentro de la sala");
+        PhotonNetwork.JoinRoom(inputSalaUnirse.text);
+        CambiarPanel(panelSala);
+        //ActualizarSala();
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         base.OnPlayerEnteredRoom(newPlayer);
+        if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            CambiarEstado("Sala Completa");
+        }
+        else
+        {
+            ActualizarSala();
+        }
     }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
+    public override void OnLeftRoom()
     {
-        base.OnPlayerLeftRoom(otherPlayer);
+        base.OnLeftRoom();
+        PhotonNetwork.LeaveRoom();
+        CambiarEstado("Has abandonado la sala");
+        CambiarPanel(panelBienvenida);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -180,9 +182,6 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         base.OnCreateRoomFailed(returnCode, message);
         CambiarEstado("Error al crear sala: " + message);
     }
-
-
-
     #endregion
 
 
@@ -197,15 +196,61 @@ public class CtrlConexion : MonoBehaviourPunCallbacks
         txtEstado.text = texto;
     }
 
-
     private void CambiarPanel (GameObject panelObjetivo)
     {
         panelBienvenida.SetActive(false);
         panelInicio.SetActive(false);
         panelCreacionSala.SetActive(false);
         panelSala.SetActive(false);
+        panelUnirseSala.SetActive(false);
 
         panelObjetivo.SetActive(true);
+    }
+
+    private void ActualizarSala()
+    {
+        // Información de los jugadores en sala
+        Player[] jugadores = PhotonNetwork.PlayerList;
+        foreach (var jugador in jugadores)
+        {
+            GameObject botonNuevo = Instantiate(cajaJugador, contenedor.transform);
+            //botonNuevo.transform.SetParent(contenedor.transform);
+            botonNuevo.transform.Find("Nickname").GetComponent<TextMeshProUGUI>().text = jugador.NickName;
+        }
+
+        // Información de la Sala
+        nombreSala.text = "Sala " + PhotonNetwork.CurrentRoom.Name.ToString();
+        tipoSala.text = (PhotonNetwork.CurrentRoom.IsOpen ? "Abierta" : "Cerrada");
+        totalJugadores.text = PhotonNetwork.CurrentRoom.PlayerCount.ToString() + "/";
+        maximoJugadores.text = PhotonNetwork.CurrentRoom.MaxPlayers.ToString();
+    }
+
+    #endregion
+
+    #region Métodos públicos
+    public void DesconectarServidor()
+    {
+        botonConectar.interactable = true;
+        inputNickname.text = string.Empty;
+        txtInfoUser.text = "No user";
+        CambiarPanel(panelInicio);
+        CambiarEstado("Desconectado de Photon");
+        PhotonNetwork.Disconnect();
+    }
+
+    public void UnirseASala()
+    {
+        
+        if (!(string.IsNullOrWhiteSpace(inputSalaUnirse.text)))
+        {
+            PhotonNetwork.JoinRoom(inputSalaUnirse.text);
+            CambiarPanel(panelSala);
+        }
+    }
+
+    public void VolverAtras()
+    {
+        CambiarPanel(panelBienvenida);
     }
     #endregion
 }
